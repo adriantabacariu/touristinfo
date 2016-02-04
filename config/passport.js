0e285885100bdef1147cba845cfcef66274f4b7e
Adrian Tabacariu <adrian.tabacariu@yahoo.com>
2016-02-04 02:47:27 +0200

    Switch users mysql logic to mongoDB
    Implemented mongoDB integration + Refactored db-related code into
    UsersRepository

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,7 @@
 var config = require('./settings');
 var bcrypt = require('bcrypt-nodejs');
-var mysql = require('mysql');
-var connection = mysql.createConnection(config.db.connection);
 var LocalStrategy = require('passport-local').Strategy;
+var usersRepository = require('../app/repositories/usersRepository');
 
 module.exports = function (passport) {
   passport.serializeUser(function (user, done) {
@@ -10,9 +9,7 @@ module.exports = function (passport) {
   });
 
   passport.deserializeUser(function (id, done) {
-    connection.query('SELECT * FROM ' + config.db.tables.users + ' WHERE id = ?', [id], function (err, rows) {
-      done(err, rows[0]);
-    });
+    usersRepository.executeDbQuery(usersRepository.findUserById, { id: id }, done);
   });
 
   passport.use('join', new LocalStrategy({
@@ -20,51 +17,48 @@ module.exports = function (passport) {
     passwordField: 'password',
     usernameField: 'username'
   },
-  function (req, username, password, done) {
-    connection.query('SELECT * FROM ' + config.db.tables.users + ' WHERE username = ?', [username], function (err, rows) {
-      if (err) {
-        return done(err);
-      }
+    function (req, username, password, done) {
+      usersRepository.executeDbQuery(usersRepository.findUserByName, { username: username }, function (err, rows) {
+        if (err) {
+          return done(err);
+        }
 
-      if (rows.length) {
-        return done(null, false, req.flash('joinMessage', 'Username already taken.'));
-      } else {
-        var newUserMysql = {
-          password: bcrypt.hashSync(password, null, null),
-          username: username
-        };
+        if (rows.length) {
+          return done(null, false, req.flash('joinMessage', 'Username already taken.'));
+        } else {
+          var userToInsert = {
+            password: password,
+            username: username
+          };
 
-        var insertQuery = 'INSERT INTO ' + config.db.tables.users + ' (username, password) VALUES (?, ?)';
-
-        connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
-          newUserMysql.id = rows.insertId;
-
-          return done(null, newUserMysql);
-        });
-      }
-    });
-  }));
+          usersRepository.executeDbQuery(usersRepository.insertUser, userToInsert, function (err, rows) {
+            userToInsert.id = rows.insertId;
+            return done(null, userToInsert);
+          });
+        }
+      });
+    }));
 
   passport.use('login', new LocalStrategy({
     passReqToCallback: true,
     passwordField: 'password',
     usernameField: 'username'
   },
-  function (req, username, password, done) {
-    connection.query('SELECT * FROM ' + config.db.tables.users + ' WHERE username = ?', [username], function (err, rows) {
-      if (err) {
-        return done(err);
-      }
+    function (req, username, password, done) {
+      usersRepository.executeDbQuery(usersRepository.findUserByName, { username: username }, function (err, rows) {
+        if (err) {
+          return done(err);
+        }
 
-      if (!rows.length) {
-        return done(null, false, req.flash('loginMessage', 'User not found.'));
-      }
+        if (!rows.length) {
+          return done(null, false, req.flash('loginMessage', 'User not found.'));
+        }
 
-      if (!bcrypt.compareSync(password, rows[0].password)) {
-        return done(null, false, req.flash('loginMessage', 'Wrong password.'));
-      }
+        if (!bcrypt.compareSync(password, rows[0].password)) {
+          return done(null, false, req.flash('loginMessage', 'Wrong password.'));
+        }
 
-      return done(null, rows[0]);
-    });
-  }));
+        return done(null, rows[0]);
+      });
+    }));
 };
